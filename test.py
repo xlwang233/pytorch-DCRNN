Correct an error for metric calculation

test.py
@@ -49,15 +49,15 @@ def main(config):
         for i, (x, y) in tqdm(enumerate(test_data_loader.get_iterator()), total=num_test_iteration):
             x = torch.FloatTensor(x).cuda()
             y = torch.FloatTensor(y).cuda()
-            outputs = model(x, y, 0)  # (seq_length+1, batch_size, num_nodes*output_dim)  (13, 50, 207*1)
+            outputs = model(x, y, 0)  # (seq_length, batch_size, num_nodes*output_dim)  (12, 50, 207*1)
             y_preds = torch.cat([y_preds, outputs], dim=1)
-    y_preds = torch.transpose(y_preds, 0, 1)
+    y_preds = torch.transpose(y_preds, 0, 1)  # (6850, 12, 207)
     y_preds = y_preds.detach().numpy()  # cast to numpy array
     print("--------test results--------")
     for horizon_i in range(y_truths.shape[1]):
-        y_truth = np.squeeze(y_truths[:, horizon_i, :, 0])
+        y_truth = np.squeeze(y_truths[:, horizon_i, :, 0])  # should be (6850, 207)
 
-        y_pred = scaler.inverse_transform(y_preds[:y_truth.shape[0], horizon_i, :])
+        y_pred = scaler.inverse_transform(y_preds[:y_truth.shape[0], horizon_i, :])  # should be (6850, 207)
         predictions.append(y_pred)
 
         mae = metrics.masked_mae_np(y_pred, y_truth, null_val=0)