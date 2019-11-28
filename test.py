Fix bugs in _valid_epoch and test. Add average training time per epoch.

test.py
@@ -7,6 +7,7 @@ from parse_config import ConfigParser
 from lib import utils
 from tqdm import tqdm
 import math
+import time
 
 
 def main(config):
@@ -38,20 +39,21 @@ def main(config):
     model = model.to(device)
     model.eval()
 
-    total_loss = 0.0
-
     y_preds = torch.FloatTensor([])
     y_truths = data['y_test']  # (6850, 12, 207, 2)
     y_truths = scaler.inverse_transform(y_truths)
     predictions = []
     groundtruth = list()
 
+    start_time = time.time()
     with torch.no_grad():
         for i, (x, y) in tqdm(enumerate(test_data_loader.get_iterator()), total=num_test_iteration):
             x = torch.FloatTensor(x).cuda()
             y = torch.FloatTensor(y).cuda()
             outputs = model(x, y, 0)  # (seq_length, batch_size, num_nodes*output_dim)  (12, 50, 207*1)
             y_preds = torch.cat([y_preds, outputs], dim=1)
+    inference_time = time.time() - start_time
+    logger.info("Inference time: {:.4f} s".format(inference_time))
     y_preds = torch.transpose(y_preds, 0, 1)
     y_preds = y_preds.detach().numpy()  # cast to numpy array
     print("--------test results--------")
@@ -62,9 +64,9 @@ def main(config):
         predictions.append(y_pred)
         groundtruth.append(y_truth)
 
-        mae = metrics.masked_mae_np(y_pred, y_truth, null_val=0)
-        mape = metrics.masked_mape_np(y_pred, y_truth, null_val=0)
-        rmse = metrics.masked_rmse_np(y_pred, y_truth, null_val=0)
+        mae = metrics.masked_mae_np(y_pred[:y_truth.shape[0]], y_truth, null_val=0)
+        mape = metrics.masked_mape_np(y_pred[:y_truth.shape[0]], y_truth, null_val=0)
+        rmse = metrics.masked_rmse_np(y_pred[:y_truth.shape[0]], y_truth, null_val=0)
         print(
             "Horizon {:02d}, MAE: {:.2f}, MAPE: {:.4f}, RMSE: {:.2f}".format(
                 horizon_i + 1, mae, mape, rmse

trainer/dcrnn_trainer.py
@@ -73,6 +73,7 @@ class DCRNNTrainer(BaseTrainer):
             # add max grad clipping
             torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
             self.optimizer.step()
+            training_time = time.time() - start_time
 
             self.writer.set_step((epoch - 1) * self.len_epoch + batch_idx)
             self.writer.add_scalar('loss', loss.item())
@@ -99,8 +100,8 @@ class DCRNNTrainer(BaseTrainer):
 
         if self.lr_scheduler is not None:
             self.lr_scheduler.step()
-        log.update({'Time': "{:.4f}s".format(time.time() - start_time)})
-        return log
+        log.update({'Time': "{:.4f}s".format(training_time)})
+        return log, training_time
 
     def _valid_epoch(self, epoch):
         """
@@ -122,8 +123,8 @@ class DCRNNTrainer(BaseTrainer):
                 data, target = data.to(self.device), target.to(self.device)
 
                 output = self.model(data, target, 0)
-                output = torch.transpose(output[1:].view(12, self.model.batch_size, self.model.num_nodes,
-                                                         self.model.output_dim), 0, 1)  # back to (50, 12, 207, 1)
+                output = torch.transpose(output.view(12, self.model.batch_size, self.model.num_nodes,
+                                                     self.model.output_dim), 0, 1)  # back to (50, 12, 207, 1)
 
                 loss = self.loss(output.cpu(), label)
 